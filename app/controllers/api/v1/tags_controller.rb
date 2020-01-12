e0e8ab09ba2877d61e2ea0fbf1aa33adc1b815dc
Alex Rupp-Coppi <alex@dossamer.io>
2020-01-12 16:04:37 -0500

    tag controller edit

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,4 +1,4 @@
-class Api:V1:TagsController < ApplicationController
+class Api::V1::TagsController < ApplicationController
   before_action :set_tag, only: [:show, :update, :destroy]
 
   # GET /tags
