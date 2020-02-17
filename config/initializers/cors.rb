2a28ddcf0e34a28c14a781884399061bc9a5ca58
Alexander Rupp-Coppi <amr2311@columbia.edu>
2020-02-16 20:39:17 -0500

    fixing cors, db permissions, transferred db to postgres

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5000', '127.0.0.1:5000', 'localhost:8080', '127.0.0.1:8080', 'scores.cuclassicalcomposers.org'
+    origins 'localhost:5000', '127.0.0.1:5000', 'localhost:8080', '127.0.0.1:8080', 'scores.cuclassicalcomposers.org',
+            '160.39.143.199' # alex dev laptop
 
     resource '*',
       headers: :any,
+      expose: ["Authorization"],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
