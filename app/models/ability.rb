47cb3c319dd429cc640e73e0a52f0a4f224a0f15
Alexander Rupp-Coppi <amr2311@columbia.edu>
2020-02-22 21:36:38 -0500

    finished first pass at cancan privileges

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,48 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-    #
-    user ||= User.new # guest user (not logged in)
+    
+    guest_user = false
+
+    if user == nil
+      guest_user = true
+      user = User.new
+    end
+
     if user.is_admin
       can :manage, :all
     elsif user.is_curator
-      can :read, :all 
+      can [:read, :show], :all
+      can :create, App
+      can :create, ApplicationComment
+      can :create, Decision
+      can [:update, :delete], ApplicationComment, author_id_id: user.id # need to filter via application author at controller level 
+      can [:update, :delete], Decision, author_id_id: user.id # filter local
+      can :create, GenreScore # filter local
+      can :create, Genre
+      can :create, Rating
+      can [:update, :delete], Rating, author_id_id: user.id
+      can :create, ScoreComment
+      can :create, ScoreTag
+      can :manage, Score, author_id_id: user.id
+      can :create, Tag
+      can :update, User, id: user.id
+    elsif !guest_user
+      can :create, App
+      can [:read, :show], App, author_id_id: user.id
+      can :read, ApplicationComment # need to filter via application author at controller level 
+      can :read, Decision # filter local
+      can [:read, :create, :show], GenreScore # filter local
+      can [:read, :show], Genre
+      can [:read, :show], Rating
+      can [:read, :show], ScoreComment
+      can [:read, :create, :show], ScoreTag
+      can :create, Score, author_id_id: user.id
+      can [:read, :create, :show], Tag
+      can :update, User, id: user.id
+      can [:read, :show], User
     else
-      # can :read, :all
+      can [:read, :show], User
     end
     #
     # The first argument to `can` is the action you are giving the user
